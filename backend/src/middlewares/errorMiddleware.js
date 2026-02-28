2dc4f23109ada23f879164cfaad8be6f8b92330e
Jesús Sandoval <jesus1721737@gmail.com>
2026-02-28 16:14:08 -0600

    Delete backend directory

--- backend/src/middlewares/errorMiddleware.js
+++
@@ -1,7 +0,0 @@
-module.exports = (err, req, res, next) => {
-  console.error("Error:", err.message);
-
-  res.status(err.status || 500).json({
-    message: err.message || "Error interno del servidor"
-  });
-};
